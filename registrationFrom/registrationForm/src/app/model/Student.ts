3cca7446a6a44800d2e09b3c81e2e6e1a617e526
Sufiyan33 <basu700736@gmail.com>
2024-11-12 20:03:53 +0530

    Created model of student

---
+++ registrationFrom/registrationForm/src/app/model/Student.ts
@@ -0,0 +1,11 @@
+export class StudentModel  {
+    name: string;
+    course: string;
+    fees: number;
+
+    constructor(){
+        this.name = '';
+        this.course = '';
+        this.fees = 0;
+    }
+}
