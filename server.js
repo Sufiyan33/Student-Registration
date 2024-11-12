05f556c2e35e7c5bc8fa6b4769ef8cf5b2c803cb
Sufiyan33 <basu700736@gmail.com>
2024-11-12 12:49:29 +0530

    updated mysql connection properties

--- server.js
+++ server.js
@@ -3,3 +3,11 @@ const bodyParser = require("body-parser");
 const server = express();
 const mysql = require("mysql");
 server.use(bodyParser.json());
+
+//Db connection
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "dataFront",
+});
