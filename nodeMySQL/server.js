3bbcbb7dc64e0ecb9573b01f311c2dd2053d2236
Sufiyan33 <basu700736@gmail.com>
2024-11-12 15:17:38 +0530

    Post endpoint for adding students

--- nodeMySQL/server.js
+++ nodeMySQL/server.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
 const server = express();
 const mysql = require("mysql2");
 server.use(bodyParser.json());
@@ -25,4 +25,21 @@ db.connect(function (error) {
 server.listen(8085, function(error){
     if(error) console.log("Error...", error);
     else console.log("Started on port: 8085")
+});
+
+// Post method
+server.post("/api/student/add", (req, res) =>{
+    let details = {
+        name: req.body.name,
+        course: req.body.course,
+        fees: req.body.fees,
+    };
+    let sql = "INSERT INTO student SET ? ";
+    db.query(sql, details, (error) =>{
+        if(error){
+            res.send({status: false ,message: "Student failed to create...", error});
+        }else{
+            res.send({status: true, message: "Student created successfully..."});
+        }
+    });
 });
