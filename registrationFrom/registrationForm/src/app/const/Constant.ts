41be1a2cd04d7f5c7d69fa5cc148d3d00fa931ad
Sufiyan33 <basu700736@gmail.com>
2024-11-12 20:25:10 +0530

    Adding api in constant file

---
+++ registrationFrom/registrationForm/src/app/const/Constant.ts
@@ -0,0 +1,11 @@
+export const Constants ={
+    VALIDATION_MESSAGE:{},
+
+    API_DETAILS:{
+        GET_API_ALL: 'http://localhost:8085/api/student/',
+        POST_API: 'http://localhost:8085/api/student/add',
+        GET_API_BY_ID: 'http://localhost:8085/api/student/:id',
+        PUT_API: 'http://localhost:8085/api/student/update/:id',
+        DELETE_API: 'http://localhost:8085/api/student/delete:id',
+    }
+}
