0ee75daa1b24fe8b6bc696a25803299e903ef1e2
Sufiyan33 <basu700736@gmail.com>
2024-11-13 10:18:05 +0530

    Logics in service class

--- registrationFrom/registrationForm/src/app/services/studentservice.service.ts
+++ registrationFrom/registrationForm/src/app/services/studentservice.service.ts
@@ -1,9 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { StudentModel } from '../model/Student';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StudentserviceService {
+  private postApi = 'http://localhost:8085/api/student/add';
+  private getApi = 'http://localhost:8085/api/student/';
+  private putApi = 'http://localhost:8085/api/student/update/:id';
+  private deleteApi = 'http://localhost:8085/api/student/delete:id';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  createStudent(student: StudentModel): Observable<StudentModel>{
+    return this.http.post<StudentModel>(this.postApi, student);
+  }
+
+  getStudents(): Observable<StudentModel[]>{
+    return this.http.get<StudentModel[]>(this.getApi);
+  }
+
+  updateStudent(student: StudentModel): Observable<StudentModel>{
+    return this.http.put<StudentModel>(`${this.putApi}/${student.id}`, student);
+  }
+
+  deleteStudent(id: number): Observable<void>{
+    return this.http.delete<void>(`${this.deleteApi}/${id}`);
+  }
 }
