2e5ef024da7e3318b90a2c1929e7038bcfe576b1
Sufiyan33 <basu700736@gmail.com>
2024-11-13 14:30:00 +0530

    Calling api directly from constant file

--- registrationFrom/registrationForm/src/app/const/Constant.ts
+++ registrationFrom/registrationForm/src/app/const/Constant.ts
@@ -2,10 +2,6 @@ export const Constants ={
     VALIDATION_MESSAGE:{},
 
     API_DETAILS:{
-        GET_API_ALL: 'http://localhost:8085/api/student/',
-        POST_API: 'http://localhost:8085/api/student/add',
-        GET_API_BY_ID: 'http://localhost:8085/api/student/:id',
-        PUT_API: 'http://localhost:8085/api/student/update/:id',
-        DELETE_API: 'http://localhost:8085/api/student/delete:id',
+        API_URL: 'http://localhost:8085/api/student'
     }
 }
--- registrationFrom/registrationForm/src/app/services/studentservice.service.ts
+++ registrationFrom/registrationForm/src/app/services/studentservice.service.ts
@@ -2,29 +2,28 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { StudentModel } from '../model/Student';
 import { map, Observable } from 'rxjs';
+import { Constants } from '../const/Constant';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StudentserviceService {
-  private apiUrl = 'http://localhost:8085/api/student';
-
   constructor(private http: HttpClient) { }
 
   createStudent(student: StudentModel): Observable<StudentModel>{
-    return this.http.post<StudentModel>(this.apiUrl, student);
+    return this.http.post<StudentModel>(Constants.API_DETAILS.API_URL, student);
   }
 
   getStudents(): Observable<StudentModel[]>{
-    return this.http.get<{status: boolean, data: StudentModel[]}>(this.apiUrl)
+    return this.http.get<{status: boolean, data: StudentModel[]}>(Constants.API_DETAILS.API_URL)
     .pipe(map(response => response.data));
   }
 
   updateStudent(id: number, student: StudentModel): Observable<any>{
-    return this.http.put(`${this.apiUrl}/${id}`, student);
+    return this.http.put(`${Constants.API_DETAILS.API_URL}/${id}`, student);
   }
 
   deleteStudent(id: number): Observable<void>{
-    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+    return this.http.delete<void>(`${Constants.API_DETAILS.API_URL}/${id}`);
   }
 }
